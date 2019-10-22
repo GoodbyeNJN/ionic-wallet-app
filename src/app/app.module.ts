884bee6a2a7ca54d9fed0618053309d8bd49d6af
陈超 <chenchao@moacchina.com>
2019-10-22 15:47:56 +0800

    chore: added providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,53 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { IonicStorageModule } from '@ionic/storage';
-import { TabsPage } from '../pages/tabs/tabs';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
+import { IonicStorageModule } from "@ionic/storage";
+import { TabsPage } from "../pages/tabs/tabs";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Clipboard } from '@ionic-native/clipboard';
-import { WalletProvider } from '../providers/wallet';
-import { NgxQRCodeModule } from 'ngx-qrcode2';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { SocialSharing } from '@ionic-native/social-sharing';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { createTranslateLoader } from '../services/language.service';
-import { AppAvailability } from '@ionic-native/app-availability';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Clipboard } from "@ionic-native/clipboard";
+import { WalletProvider } from "../providers/wallet";
+import { NgxQRCodeModule } from "ngx-qrcode2";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { SocialSharing } from "@ionic-native/social-sharing";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { createTranslateLoader } from "../services/language.service";
+import { AppAvailability } from "@ionic-native/app-availability";
+import { FileTransfer } from "@ionic-native/file-transfer";
+import { Camera } from "@ionic-native/camera";
+import { Crop } from "@ionic-native/crop";
+
 @NgModule({
-  declarations: [
-    MyApp,
-    TabsPage
-  ],
+  declarations: [MyApp, TabsPage],
   imports: [
     BrowserModule,
     HttpClientModule,
     NgxQRCodeModule,
     IonicModule.forRoot(MyApp, {
-      backButtonText: '',//按钮内容
+      backButtonText: "", //按钮内容
       backButtonIcon: "ios-arrow-back",
       iconMode: "ios",
       tabsPlacement: "bottom",
-      modalEnter: 'modal-slide-in',
-      modalLeave: 'modal-slide-out',
+      modalEnter: "modal-slide-in",
+      modalLeave: "modal-slide-out",
       // pageTransition: 'ios-transition',
-      tabsHideOnSubPages: 'true'
+      tabsHideOnSubPages: "true",
     }),
     // 调用forRoot静态方法指定加载的文件
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
     }),
-    IonicStorageModule.forRoot()
-
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    TabsPage
-  ],
+  entryComponents: [MyApp, TabsPage],
   providers: [
     StatusBar,
     SplashScreen,
@@ -59,7 +56,10 @@ import { AppAvailability } from '@ionic-native/app-availability';
     WalletProvider,
     BarcodeScanner,
     SocialSharing,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
-  ]
+    FileTransfer,
+    Camera,
+    Crop,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+  ],
 })
-export class AppModule { }
+export class AppModule {}

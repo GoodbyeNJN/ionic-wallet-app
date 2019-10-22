187120444a64821a681dc7b200f298a86f85255d
陈超 <chenchao@moacchina.com>
2019-10-22 23:19:09 +0800

    chore: added global config file

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  domain: "http://qd-wallet.moacchina.info",
+  api: {
+    upload: "/api/up",
+    getInfo: "/api/find",
+    version: "/api/version",
+  },
+  upgrade: "/download",
+  // rpcUrl: "http://testnode.moacchina.info",
+  rpcUrl: "http://node.moacchina.info",
+};
